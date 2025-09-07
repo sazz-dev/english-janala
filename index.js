20650fced2fc88c16f4f980a383604436cf53699
sazz-dev <sazzadur.rahman193@gmail.com>
2025-09-07 16:53:57 +0600

    done

--- index.js
+++ index.js
@@ -4,38 +4,6 @@ const loadlessons = () => {
     .then((json) => displaylesson(json.data));
 };
 
-const loadLevelWord = (id) => {
-  const url = `https://openapi.programming-hero.com/api/level/${id}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => displayLevelWord(data.data));
-};
-
-const displayLevelWord = (words) => {
-  const WordContainer = document.getElementById("word-container");
-  WordContainer.innerHTML = "";
-
-  words.forEach((word) => {
-
-    const card = document.createElement("div");
-    card.innerHTML = `
-        
-                <div class="rounded-2xl bg-white p-8 flex gap-3 flex-col items-center hover:shadow-lg transform transition duration-300 hover:scale-105">
-                <h2 class="font-semibold text-[32px]">${word.word}</h2>
-                <p class="text-[18px] md:text-[20px] text-center">Meaning /Pronounciation</p>
-                <p class="text-[28px]">${word.meaning} / ${word.pronunciation}</p>
-                <div class=" w-full flex justify-between">
-                    <button class="w-12 h-12 bg-[#EDF0F0] text-2xl rounded-full"><i class="text-[#345C54] fa-solid fa-circle-info"></i></button>
-                    <button class="w-12 h-12 bg-[#EDF0F0] text-xl rounded-full"><i class="text-[#345C54] fa-solid fa-volume-high"></i></button>
-                </div>
-            </div>
-        
-        `;
-
-        WordContainer.append(card);
-  });
-};
-
 const displaylesson = (lessons) => {
   // 1.Get the container & Empty
   const levelContainer = document.getElementById("level-container");
@@ -46,7 +14,7 @@ const displaylesson = (lessons) => {
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
     
-    <button onclick="loadLevelWord(${lesson.level_no})" class="active:bg-[#345C54] text-[#345C54] hover:text-[#345C54] active:text-white border-1 border-[#345C54] rounded-full px-6 p-3 "><i class="pr-5 fa-solid fa-bookmark"></i>Lesson - ${lesson.level_no}</button>
+    <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline lesson-btn rounded-full text-[#345C54] font-normal text-[18px]"><i class="pr-5 fa-solid fa-bookmark"></i>Lesson - ${lesson.level_no}</button>
     
     `;
 
@@ -54,4 +22,139 @@ const displaylesson = (lessons) => {
     levelContainer.append(btnDiv);
   }
 };
+
+const reomoveActive = () => {
+  const lessonButton = document.querySelectorAll(".lesson-btn");
+  lessonButton.forEach((btn) => btn.classList.remove("active"));
+};
+
+const loadLevelWord = (id) => {
+  manageSpinner(true);
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      reomoveActive(); //Remove All active class
+      const clickBtn = document.getElementById(`lesson-btn-${id}`);
+      clickBtn.classList.add("active"); //Add only in the acive
+      displayLevelWord(data.data);
+    });
+};
+
+const displayLevelWord = (words) => {
+  const WordContainer = document.getElementById("word-container");
+  WordContainer.innerHTML = "";
+
+  if (words.length == 0) {
+    WordContainer.innerHTML = `            
+    
+                <img class="w-30 mx-auto" src="./assets/favicon.png" alt="">
+                <div class="text-center m-10 mx-a">
+                <p>এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                <h2 class="text-[32px] mt-5">নেক্সট Lesson এ যান</h2>
+            </div>`;
+
+            manageSpinner(false)
+
+    return;
+  }
+
+  words.forEach((word) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+        
+                <div class="rounded-2xl bg-white p-8 flex gap-3 flex-col items-center hover:shadow-lg transform transition duration-300 hover:scale-105">
+                <h2 class="font-semibold text-[32px]">${
+                  word.word ? word.word : "শব্দ পাওয়া যাইনি"
+                }</h2>
+                <p class="text-[18px] md:text-[20px] text-center">Meaning /Pronounciation</p>
+                <p class="text-[28px]">${
+                  word.meaning ? word.meaning : "অর্থ পাওয়া যাইনি"
+                } / ${
+      word.pronunciation ? word.pronunciation : "Pawa Jaini"
+    }</p>
+
+                <div class=" w-full flex justify-between">
+                    <button onclick="loadWordDetail(${
+                      word.id
+                    })" class="w-12 h-12 bg-[#EDF0F0] text-2xl rounded-full"><i class="text-[#345C54] fa-solid fa-circle-info"></i></button>
+
+                    <button class="w-12 h-12 bg-[#EDF0F0] text-xl rounded-full"><i class="text-[#345C54] fa-solid fa-volume-high"></i></button>
+                </div>
+            </div>
+        
+        `;
+
+    WordContainer.append(card);
+  });
+
+  manageSpinner(false);
+};
+
+// Make Span
+
+const createElements = (arr) => {
+  const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
+  return htmlElements.join("");
+};
+
+// Loader
+const manageSpinner = (status) => {
+  if (status == true) {
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("word-container").classList.add("hidden");
+  } else {
+    document.getElementById("word-container").classList.remove("hidden");
+    document.getElementById("spinner").classList.add("hidden");
+  }
+};
+
+// Make Modal Content
+
+const loadWordDetail = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetail(details.data);
+};
+
+const displayWordDetail = (word) => {
+  console.log(word);
+  const detailsBox = document.getElementById("details-container");
+  detailsBox.innerHTML = `
+
+
+
+    <div class="">
+    <h2 class="text-2xl font-bold">${
+      word.word
+    } (<i class="fa-solid fa-microphone-lines"></i> : ${word.pronunciation}) 
+    </h2>
+    </div>
+
+    <div class="">
+    <h2 class="font-bold">${word.meaning}</h2>
+    <p>আগ্রহী</p>
+    </div>
+
+    <div class="">
+    <h2 class="font-bold">Example</h2>
+    <p>${word.sentence}</p>
+    </div>
+
+    <div class="">
+    <h2 class="font-bold">Synonym</h2>
+    <div class="">${createElements(word.synonyms)}</div>
+    </div>
+    
+
+    </div>
+    
+
+
+  `;
+
+  document.getElementById("word_modal").showModal();
+};
+
 loadlessons();
